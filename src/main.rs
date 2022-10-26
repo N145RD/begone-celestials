e61521ad88e9faec6cc2ceb25146c30d56309aec
Seal <n145rd@tutanota.com>
2022-10-26 23:03:15 +0200

    Up to search page query and added scraper

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use reqwest::*;
+use scraper::*;
 
 fn main() {
     get_id("test");
 }
 
 fn get_id(name: &'static str) -> Result<&str> {
-    let url = "ratingupdate.info/?name={}".to_owned() + name;
-    let namelist = reqwest::blocking::get(url);
+    let url = "http://www.ratingupdate.info/?name=".to_owned() + name;
+    let namelist = reqwest::blocking::get(url)?.text();
 
     println!("{:#?}", namelist);
     Ok("ok")
