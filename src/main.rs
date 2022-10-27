eda2a65cbfd0b1424f892e56cc05096bc17de618
Seal <n145rd@tutanota.com>
2022-10-27 20:14:03 +0200

    Separation of request results

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
 use reqwest::*;
 use scraper::*;
 
-fn main() {
-    get_id("test");
+#[derive(Debug)]
+struct Search {
+    name: String,
+    id: String,
+    character: String
 }
 
-fn get_id(target_name: &'static str) -> Result<&str> {
+fn main() {
+    get_id("Nemi");
+}
+
+fn get_id(target_name: &'static str) -> Result<Vec<Search>> {
     let url = "http://www.ratingupdate.info/?name=".to_owned() + target_name;
     let body = reqwest::blocking::get(url)?.text()?;
     let document = Html::parse_document(&body);
+    let mut ret_value: Vec<Search> = Vec::new();
 
     let selector = Selector::parse(r#"a[class="maybe_long_name"]"#).unwrap();
     for entry in document.select(&selector) {
         let name = entry.value().attr("title").ok_or("");
         let url = entry.value().attr("href").ok_or("");
         if name.unwrap() == target_name {
-            println!("Found matching name : {} at link {}", name.unwrap(), url.unwrap());
+            ret_value.push(Search{
+                name: name.unwrap().to_string(),
+                id: url.unwrap().replace("/player/", "")[..15].to_string(),
+                character: url.unwrap()[24..].to_string()
+            });
         }
     }
-    Ok("ok")
+    println!("{:?}", ret_value);
+    Ok(ret_value)
 }
