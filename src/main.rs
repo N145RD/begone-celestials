8069182382464617bb3438f333cda03a43368a27
Seal <n145rd@tutanota.com>
2022-10-27 19:41:14 +0200

    Can get names and urls

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,18 @@ fn main() {
     get_id("test");
 }
 
-fn get_id(name: &'static str) -> Result<&str> {
-    let url = "http://www.ratingupdate.info/?name=".to_owned() + name;
-    let namelist = reqwest::blocking::get(url)?.text();
+fn get_id(target_name: &'static str) -> Result<&str> {
+    let url = "http://www.ratingupdate.info/?name=".to_owned() + target_name;
+    let body = reqwest::blocking::get(url)?.text()?;
+    let document = Html::parse_document(&body);
 
-    println!("{:#?}", namelist);
+    let selector = Selector::parse(r#"a[class="maybe_long_name"]"#).unwrap();
+    for entry in document.select(&selector) {
+        let name = entry.value().attr("title").ok_or("");
+        let url = entry.value().attr("href").ok_or("");
+        if name.unwrap() == target_name {
+            println!("Found matching name : {} at link {}", name.unwrap(), url.unwrap());
+        }
+    }
     Ok("ok")
 }
